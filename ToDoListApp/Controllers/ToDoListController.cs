8ecf470e35e4ddede1a568946fb18c2b3b768886
georgeolson92 <georgeolson92@gmail.com>
2016-09-06 11:31:21 -0700

    Add project files with routes

---
+++ ToDoListApp/Controllers/ToDoListController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ToDoListApp.Controllers
+{
+    public class ToDoListController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
